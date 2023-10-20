02e29554b6a84fd696dd437119727402153d7f85
Kaya Chakmak <kycakmak@gmail.com>
2023-10-20 17:04:09 +0200

    create all elements in JS including buttons, search bar, pagination

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 import { createCharacterCard } from "./components/card/card.js";
+import { createSearchBar } from "./components/search-bar/search-bar.js";
+import { createElementWithClassAndData } from "./components/search-bar/search-bar.js";
+import { createButton } from "./components/nav-button/nav-button.js";
+import { createPagination } from "./components/nav-pagination/nav-pagination.js";
+
+const main = document.querySelector('[data-js="main"]');
+
+const searchBar = createSearchBar();
+
+const CardContainer = createElementWithClassAndData(
+  "ul",
+  "card-container",
+  "card-container"
+);
+main.append(searchBar, CardContainer);
+const Navigation = createElementWithClassAndData(
+  "nav",
+  "navigation",
+  "navigation"
+);
+main.insertAdjacentElement("afterend", Navigation);
+
+const PrevButton = createButton("previous", "button--prev", "button-prev");
+const pagElement = createPagination();
+const NextButton = createButton("next", "button--next", "button-next");
+
+Navigation.append(PrevButton, pagElement, NextButton);
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
-const searchBar = document.querySelector('[data-js="search-bar"]');
-const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
+
 let maxPage = 42;
 let page = 1;
 let searchQuery = "";
+
 pagination.textContent = `${page} / ${maxPage} `;
 
 let URL = `https://rickandmortyapi.com/api/character/?name=${searchQuery}&?page=${page}`;
